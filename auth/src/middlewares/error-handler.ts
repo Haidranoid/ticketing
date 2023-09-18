ec5156c1a5245f03a6e3de20fa5a549c9e209454
Jose Eduardo Martinez Lara <martinezlara_joseeduardo@hotmail.com>
2023-09-17 21:23:32 -0600

    secrets in kubernetes added

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -8,13 +8,15 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   if (err instanceof CustomError) {
-    return res.status(err.statusCode)
+    return res
+      .status(err.statusCode)
       .send({
         errors: err.serializeErrors()
       });
   }
 
-  res.status(400)
+  return res
+    .status(400)
     .send({
       errors: [{ message: "Something went wrong" }],
     });

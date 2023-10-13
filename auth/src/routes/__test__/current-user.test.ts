b9e78da0ec9a344c22b6fdef1a2bfc3bacc8484a
Jose Eduardo Martinez Lara <martinezlara_joseeduardo@hotmail.com>
2023-10-12 18:59:50 -0600

    last minute change

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -8,7 +8,7 @@ it('should respond with details about te current user', async function () {
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
     .send({})
-    .expect(200)
+    .expect(400)
 
   expect(response.body.currentUser.email).toEqual('test@test.com')
 });
